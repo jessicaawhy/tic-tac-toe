9f4b1a813d074123c7ec8cc93a10bd5405be033f
Jessica Nguyen <jessicaawhy@gmail.com>
2020-09-18 13:49:59 -0700

    Build functions to allow for placing markers on the board

--- main.js
+++ main.js
@@ -15,11 +15,12 @@ const player2 = playerFactory('Player 2', 'O');
 
 // displayController
 const displayController = (()=>{
-  
+  let marker = 'X';
+  return {marker};
 })()
 
 const board = document.querySelector('#gameboard')
-window.addEventListener('load', render)
+render();
 // board.addEventListener('click', render)
 
 function render() {
@@ -31,10 +32,32 @@ function render() {
     row.style.display = 'flex';
     for (let j = 0; j < 3; j++) {
       let element = document.createElement('div');
-      element.classList.add('box')
+      element.classList.add('box');
+      element.setAttribute('id', `row${i}column${j}`);
       element.innerHTML = gameBoard.board[i][j] || '';
       row.appendChild(element);
     }
     board.appendChild(row);
+  }
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach(box => box.addEventListener('click', updateGameBoard));
+}
+
+function updateGameBoard(e) {
+  let row = e.srcElement.id[3];
+  let column = e.srcElement.id[10];
+
+  if (!gameBoard.board[row][column]) {
+    gameBoard.board[row][column] = displayController.marker;
+    render();
+    switchMarker();
+  }
+}
+
+function switchMarker(){
+  if (displayController.marker === 'X') {
+    displayController.marker = 'O';
+  } else {
+    displayController.marker = 'X';
   }
 }
