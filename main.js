fb8d01a4c7d56d32f0e6c69f3cdcf9591dc1c02c
Jessica Nguyen <jessicaawhy@gmail.com>
2020-09-18 16:58:54 -0700

    Reorganize functions into a factory function to minimize global code

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-
 // gameBoard
 const gameBoard = (() => {
   const board = [[,,], [,,], [,,]];
   return {board};
 })()
+
+const board = document.querySelector('#gameboard')
 
 // Players
 const playerFactory = (name, marker) => {
@@ -16,48 +17,44 @@ const player2 = playerFactory('Player 2', 'O');
 // displayController
 const displayController = (()=>{
   let marker = 'X';
-  return {marker};
-})()
-
-const board = document.querySelector('#gameboard')
-render();
-// board.addEventListener('click', render)
-
-function render() {
-  while (board.firstChild) {
-    board.removeChild(board.firstChild);
-  }
-  for (let i = 0; i < 3; i++) {
-    let row = document.createElement('div');
-    row.style.display = 'flex';
-    for (let j = 0; j < 3; j++) {
-      let element = document.createElement('div');
-      element.classList.add('box');
-      element.setAttribute('id', `row${i}column${j}`);
-      element.innerHTML = gameBoard.board[i][j] || '';
-      row.appendChild(element);
+  
+  const render = () => {
+    while (board.firstChild) {
+      board.removeChild(board.firstChild);
     }
-    board.appendChild(row);
+    for (let i = 0; i < 3; i++) {
+      let row = document.createElement('div');
+      row.style.display = 'flex';
+      for (let j = 0; j < 3; j++) {
+        let element = document.createElement('div');
+        element.classList.add('box');
+        element.setAttribute('id', `row${i}column${j}`);
+        element.innerHTML = gameBoard.board[i][j] || '';
+        row.appendChild(element);
+      }
+      board.appendChild(row);
+    }
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('click', updateGameBoard));
   }
-  const boxes = document.querySelectorAll('.box');
-  boxes.forEach(box => box.addEventListener('click', updateGameBoard));
-}
 
-function updateGameBoard(e) {
-  let row = e.srcElement.id[3];
-  let column = e.srcElement.id[10];
-
-  if (!gameBoard.board[row][column]) {
-    gameBoard.board[row][column] = displayController.marker;
-    render();
-    switchMarker();
+  const switchMarker = () => {
+    if (marker === 'X') {
+      marker = 'O';
+    } else {
+      marker = 'X';
+    }
   }
-}
 
-function switchMarker(){
-  if (displayController.marker === 'X') {
-    displayController.marker = 'O';
-  } else {
-    displayController.marker = 'X';
+  const updateGameBoard = (e) => {
+    let row = e.srcElement.id[3];
+    let column = e.srcElement.id[10];
+    if (!gameBoard.board[row][column]) {
+      gameBoard.board[row][column] = marker;
+      render();
+      switchMarker();
+    }
   }
-}
+
+  render()
+})()
