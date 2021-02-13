ac31421446cbcbfeff5f6fa65eafcf3922d951fc
Jessica Nguyen <jessicaawhy@gmail.com>
2021-02-12 20:36:47 -0800

    Make game module and render moves on click

--- src/index.js
+++ src/index.js
@@ -1,28 +1,55 @@
 import './styles/style.css';
 
-const board = document.querySelector('#gameboard');
-const currentGame = Array.from({length: 9}, () => null);
+const displayController = (()=>{
+  const board = document.querySelector('#gameboard');
 
-currentGame.forEach((x, i) => {
-  const square = document.createElement('div');
-  square.classList.add('square');
+  let xIsNext = true;
+  let currentGame = Array.from({length: 9}, () => null);
 
-  if (i < 3) {
-    square.classList.add('first-row');
-  } else if (i < 6) {
-    square.classList.add('second-row');
-  } else {
-    square.classList.add('third-row');
+  const render = () => {
+    while (board.firstChild) {
+      board.removeChild(board.firstChild);
+    }
+
+    currentGame.forEach((x, i) => {
+      const square = document.createElement('div');
+
+      if (i < 3) {
+        square.classList.add('first-row');
+      } else if (i < 6) {
+        square.classList.add('second-row');
+      } else {
+        square.classList.add('third-row');
+      }
+    
+      if (i % 3 === 0) {
+        square.classList.add('first-column');
+      } else if (i % 3 === 1) {
+        square.classList.add('second-column');
+      } else {
+        square.classList.add('third-column');
+      }
+
+      square.classList.add('square');
+    
+      square.textContent = x;
+      square.dataset.index = i;
+
+      board.appendChild(square);
+
+      square.addEventListener('click', handleClick);
+    })
   }
 
-  if (i % 3 === 0) {
-    square.classList.add('first-column');
-  } else if (i % 3 === 1) {
-    square.classList.add('second-column');
-  } else {
-    square.classList.add('third-column');
+  const handleClick = (e) => {
+    if (!e.target.textContent) {
+      const index = e.target.dataset.index;
+      currentGame[index] = xIsNext ? 'X' : 'O';
+
+      xIsNext = !xIsNext;
+      render();
+    }
   }
 
-  square.textContent = x;
-  board.appendChild(square);
-})
+  render();
+})()
