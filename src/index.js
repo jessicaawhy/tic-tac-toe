e2c50f1a65bdd16978e63c44d8a4b20afeb8eabe
Jessica Nguyen <jessicaawhy@gmail.com>
2021-02-12 20:08:09 -0800

    Create gameboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './styles/style.css';
+
+const board = document.querySelector('#gameboard');
+const currentGame = Array.from({length: 9}, () => null);
+
+currentGame.forEach((x, i) => {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  if (i < 3) {
+    square.classList.add('first-row');
+  } else if (i < 6) {
+    square.classList.add('second-row');
+  } else {
+    square.classList.add('third-row');
+  }
+
+  if (i % 3 === 0) {
+    square.classList.add('first-column');
+  } else if (i % 3 === 1) {
+    square.classList.add('second-column');
+  } else {
+    square.classList.add('third-column');
+  }
+
+  square.textContent = x;
+  board.appendChild(square);
+})
