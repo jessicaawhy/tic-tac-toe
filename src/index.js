b7923be9d5c467a8205542ec0015904378c744d5
Jessica Nguyen <jessicaawhy@gmail.com>
2021-02-13 20:33:51 -0800

    Add 1 player mode

--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
 import './styles/style.css';
-import { checkGameWinner, checkNoMovesLeft } from '../src/logic';
+import { checkGameWinner, checkNoMovesLeft, returnEmptyIndices } from '../src/logic';
 import { removeBoard, createBoard, renderMessage } from './display';
 
 const displayController = (() => {
   const messageContainer = document.getElementById('message');
   const resetButton = document.getElementById('reset');
+  const modeButtons = document.querySelectorAll('.mode')
 
   let xIsNext = true;
   let currentGame = Array.from({length: 9}, () => null);
   let winner = undefined;
   let noSquaresLeft = false;
+  let players = 2;
 
   const render = () => {
+    removeListeners();
     removeBoard();
     createBoard(currentGame);
     addListeners();
 
     messageContainer.textContent = renderMessage(winner, noSquaresLeft, xIsNext);
+
+    if (
+      !winner &&
+      !noSquaresLeft &&
+      players === 1
+    ) {
+      setTimeout(computersTurn, 1000);
+    }
   }
 
-  const addListeners = () => {
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.addEventListener('click', handleClick));
+  const computersTurn = () => {
+    const freeMoves = returnEmptyIndices(currentGame);
+    const randomIndex = Math.floor(Math.random() * freeMoves.length);
+    const chosenMove = freeMoves[randomIndex];
+
+    currentGame[chosenMove] = xIsNext ? 'X' : 'O';
+
+    updateState();
+    removeListeners();
+    removeBoard();
+    createBoard(currentGame);
+    addListeners();
+
+    messageContainer.textContent = renderMessage(winner, noSquaresLeft, xIsNext);
   }
 
   const handleClick = (e) => {
@@ -33,10 +55,7 @@ const displayController = (() => {
       const index = e.target.dataset.index;
       currentGame[index] = xIsNext ? 'X' : 'O';
 
-      xIsNext = !xIsNext;
-      winner = checkGameWinner(currentGame);
-      noSquaresLeft = checkNoMovesLeft(currentGame);
-
+      updateState();
       render();
     }
   }
@@ -50,6 +69,27 @@ const displayController = (() => {
     render();
   }
 
+  const updateState = () => {
+    xIsNext = !xIsNext;
+    winner = checkGameWinner(currentGame);
+    noSquaresLeft = checkNoMovesLeft(currentGame);
+  }
+
+  const addListeners = () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('click', handleClick));
+  }
+
+  const removeListeners = () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.removeEventListener('click', handleClick));
+  }
+
+  const updateMode = (e) => {
+    players = parseInt(e.target.dataset.mode);
+  }
+
+  modeButtons.forEach(button => button.addEventListener('click', updateMode));
   resetButton.addEventListener('click', reset);
 
   render();
